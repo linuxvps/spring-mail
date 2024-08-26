6bbe2d7009b28dd7a3bfe36cc46f605175ec627f
NIMA <linux.vps.vpn@gmail.com>
2024-08-25 19:32:41 -0700

    init

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class Demo2Application {
+    @Autowired
+    private EmailService emailService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail() {
+        if (false)
+            emailService.sendEmail("linux.vps.vpn@gmail.com", "hi", "test mail");
+    }
+
+}
