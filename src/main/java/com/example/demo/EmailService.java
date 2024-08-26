4b4fe96d19a4fccf79cd6549f84ee4d5dd30436a
NIMA <linux.vps.vpn@gmail.com>
2024-08-25 20:01:29 -0700

    send Email With Attachment

--- src/main/java/com/example/demo/EmailService.java
+++ src/main/java/com/example/demo/EmailService.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
