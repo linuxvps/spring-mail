9991ba20faed86395065bf81abba70480eabd1fa
NIMA <linux.vps.vpn@gmail.com>
2024-09-02 00:44:43 -0700

    change EmailRequest to record

--- src/main/java/com/example/mail/controller/MailController.java
+++ src/main/java/com/example/mail/controller/MailController.java
@@ -27,26 +27,26 @@ public class MailController {
 
     @PostMapping("/send-email")
     public ResponseEntity<ApiResponse> sendEmail(@Valid @RequestBody EmailRequest request) {
-        emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+//        emailService.sendEmail(request.to(), request.subject(), request.body());
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(true, "Email sent successfully!"));
     }
 
     @PostMapping("/send-email-with-attachment")
     public ResponseEntity<ApiResponse> sendEmailWithAttachment(@RequestBody EmailRequest request) throws IOException {
-        if (request.getFile() == null || request.getFile().isEmpty()) {
+        if (request.file() == null || request.file().isEmpty()) {
             return ResponseEntity.badRequest().body(new ApiResponse(false, "Attachment is required"));
         }
 
-        String fileName = request.getFile().getOriginalFilename();
+        String fileName = request.file().getOriginalFilename();
         if (fileName == null) {
             return ResponseEntity.badRequest().body(new ApiResponse(false, "Invalid file name"));
         }
 
         File attachment = File.createTempFile(fileName, ".tmp");
-        request.getFile().transferTo(attachment);
+        request.file().transferTo(attachment);
 
         try {
-            emailService.sendEmailWithAttachment(request.getTo(), request.getSubject(), request.getBody(), attachment);
+            emailService.sendEmailWithAttachment(request.to(), request.subject(), request.subject(), attachment);
             attachment.delete();
             return ResponseEntity.ok(new ApiResponse(true, "Email with attachment sent successfully!"));
         } catch (Exception e) {
--- src/main/java/com/example/mail/request/EmailRequest.java
+++ src/main/java/com/example/mail/request/EmailRequest.java
@@ -3,27 +3,19 @@ package com.example.mail.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class EmailRequest {
+public record EmailRequest(
+        @NotBlank(message = "Recipient email is mandatory")
+        @Email(message = "Invalid email format")
+        String to,
 
-    @NotBlank(message = "Recipient email is mandatory")
-    @Email(message = "Invalid email format")
-    private String to;
+        @NotBlank(message = "Subject is mandatory")
+        @Size(max = 100, message = "Subject cannot be longer than 100 characters")
+        String subject,
 
-    @NotBlank(message = "Subject is mandatory")
-    @Size(max = 100, message = "Subject cannot be longer than 100 characters")
-    private String subject;
+        @NotBlank(message = "Body is mandatory")
+        String body,
 
-    @NotBlank(message = "Body is mandatory")
-    private String body;
-
-    private MultipartFile file;
-
-}
+        MultipartFile file
+) {}
