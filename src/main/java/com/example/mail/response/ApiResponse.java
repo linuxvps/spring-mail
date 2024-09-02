2363b01ad734ad83b50c529320206998986f6209
NIMA <linux.vps.vpn@gmail.com>
2024-09-01 23:57:26 -0700

    change ApiResponse to record

--- src/main/java/com/example/mail/response/ApiResponse.java
+++ src/main/java/com/example/mail/response/ApiResponse.java
@@ -1,14 +1,3 @@
 package com.example.mail.response;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class ApiResponse {
-    private boolean success;
-    private String message;
-}
+public record ApiResponse(boolean success, String message) {}
